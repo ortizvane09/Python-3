5ae9748f5cb83d4f19a0440e921879637d028ca2
ortizvane09 <vanessa5691zen@gmail.com>
2023-09-05 09:45:43 -0500

    update

--- PyPoll/Election_Data.py
+++ PyPoll/Election_Data.py
@@ -1,3 +1,43 @@
 import csv
 import os
 election_csv = os.path.join('Resources', 'election_data.csv')
+Total_Votes = 0
+Candidate_Votes = {}
+Candidates = []
+
+with open(election_csv) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=',')
+
+    next(csvreader)
+
+    for row in csvreader:
+        Total_Votes += 1
+        Candidate = row[2]
+
+        if Candidate not in Candidates:
+            Candidates.append(Candidate)
+            Candidate_Votes[Candidate] = 1
+        else:
+            Candidate_Votes[Candidate] += 1
+
+Winner = ""
+Max_Votes = 0
+
+print("Election results")
+print("--------------------------------")
+print(f"Total Votes: {Total_Votes}")
+print("--------------------------------")
+
+for Candidate in Candidates:
+    Votes = Candidate_Votes[Candidate]
+    Percentage = (Votes / Total_Votes) * 100
+    print(f"{Candidate}: {Percentage:.3f}% ({Votes})")
+
+    if Votes > Max_Votes:
+        Max_Votes = Votes
+        Winner = Candidate
+
+print("------------------------------------------------")
+
+print(f"Winner: {Winner}")
+print("------------------------------------------------")
